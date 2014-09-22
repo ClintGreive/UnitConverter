3c94c86c10cd848ac456af4fe3ea97d7572833ec
Clint Greive <clintgreive@Clints-MacBook-Air.local>
2014-09-23 08:12:36 +1000

    Initial Commit

---
+++ UnitConverter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UnitConverter
+//
+//  Created by Clint Greive on 23/09/2014.
+//  Copyright (c) 2014 Clint Greive. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
